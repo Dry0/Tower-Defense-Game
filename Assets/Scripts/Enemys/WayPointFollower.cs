bd6c490cd6b51eedf73b2c9889d6b96bd482b2fd
Ruben <115462366+Dry0@users.noreply.github.com>
2023-09-18 17:17:13 +0200

    morgen verder
    vragen naar tutorial

--- Assets/Scripts/Enemys/WayPointFollower.cs
+++ Assets/Scripts/Enemys/WayPointFollower.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class WayPointFollower : MonoBehaviour
 {
+    //waypoints: [Serializefields] 
+    private Transform[] waypoints;
+
     // Start is called before the first frame update
     void Start()
     {
