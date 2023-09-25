68423cdb125c12bc36447b04426bbb9a3d6f23c5
Ruben <115462366+Dry0@users.noreply.github.com>
2023-09-25 17:28:24 +0200

    enemy path finished
    started working on tower

--- Assets/Scripts/Enemys/WayPointFollower.cs
+++ Assets/Scripts/Enemys/WayPointFollower.cs
@@ -5,41 +5,36 @@ using UnityEngine;
 
 public class WayPointFollower : MonoBehaviour
 {
-    [SerializeField] public Transform[] waypoints;
     [SerializeField] private float speed = 1;
     [SerializeField] private int nextWaypointIndex = 1;
     [SerializeField] private float reachedWaypointClearance = 0.25f;
     [SerializeField] private Path path;
 
+    void Awake()
+    {// find an object with the type path so in this case it uses the path script very simple i know
+        path = FindObjectOfType<Path>();
+    }
+
     // Start is called before the first frame update
     void Start()
     { // make the enemy set to the first waypoint
-        transform.position = waypoints[0].position;
+        transform.position = path.waypoints[0].position;
     }
 
     // Update is called once per frame
     void Update()
     { //make the enemy move towrads the waypoints
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[nextWaypointIndex].position, Time.deltaTime * speed);
+        transform.position = Vector3.MoveTowards(transform.position, path.waypoints[nextWaypointIndex].position, Time.deltaTime * speed);
+
         //Check for the distance between the Enemy and the next waypoint is lesser than the newly created reachedWaypointClearance
-        if (Vector3.Distance(transform.position, waypoints[nextWaypointIndex].position) <= reachedWaypointClearance)
+        if (Vector3.Distance(transform.position, path.waypoints[nextWaypointIndex].position) <= reachedWaypointClearance)
         {
             nextWaypointIndex+= 1;
         }
         // is greater or equal then the amount of waypoints
-        if (nextWaypointIndex >= waypoints.Length) 
+        if (nextWaypointIndex >= path.waypoints.Length) 
         {
             nextWaypointIndex = 0;
-        }
-    }
-
-    public class FindAnyObjectByType : MonoBehaviour
-    {
-        private FindAnyObjectByType target;
-
-        private void Awake()
-        {
-            //path = FindObjectOfType<Path>();
         }
     }
 }
