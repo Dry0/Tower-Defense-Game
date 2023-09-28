79323c62604a5409759b519bbdf050e2db59cfc5
Ruben <115462366+Dry0@users.noreply.github.com>
2023-09-28 13:15:54 +0200

    finished the rotation  and tracking of tower
    working on tower damaging enemy

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Tower : MonoBehaviour
 {
+    //Transform target;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,12 +19,27 @@ public class Tower : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {  //If there are no targets there is no way for the tower to shoot at a target
         GameObject[] targets = GameObject.FindGameObjectsWithTag("Enemy");
 
-        if (targets.Length == 0)
+        if (targets.Length == 0) // stops tower if there is no target
         {
             return;
         }
+
+        target = targets[0].transform;
+
+        LookAtTarget();
     }
+
+    public void LookAtTarget()
+    {
+        Vector2 direction = target.position - transform.position;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+    }
+
+    //public bool IEnumerator Shoot()  
+    //{
+        //StartCoroutine(Shoot());
+    //}
 }
