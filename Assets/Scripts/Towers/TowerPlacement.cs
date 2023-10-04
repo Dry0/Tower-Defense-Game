cd64452535aabc24c4e27dc330d84be5115f4e3d
Ruben <115462366+Dry0@users.noreply.github.com>
2023-10-04 15:29:22 +0200

    verder aan tower gewerkt
    waaarschijnlijk een if statement verkeerd gezet

--- Assets/Scripts/Towers/TowerPlacement.cs
+++ Assets/Scripts/Towers/TowerPlacement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TowerPlacement : MonoBehaviour
 {
     [SerializeField] private GameObject towerPrefab; // stores the towerPrefab
+    [SerializeField] private GameObject[] towerSlots; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,16 @@ public class TowerPlacement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { // checks if we clicked on one if the towerslots
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // shoot a Raycast from the mousePosition to the world
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+        }
+
+        //if (hit.collider != null) //checks if the raycast hits something in the scene
+        //{
+
+        //}
     }
 }
